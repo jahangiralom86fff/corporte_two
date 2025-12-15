d61e4967e4e8cf9a31cede11f5aef7d1f156a3fb
jahangiralom86ffff <jahangiralom144155160@gmail.com>
2025-12-15 17:42:15 +0600

    new js file add all changes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,3 +7,12 @@ $(document).ready(function () {
     });
 });
 
+window.addEventListener("scroll", function (){
+
+    const navbar = document .querySelector(".header .main-navbar");
+    if( window .scrollY > 40){
+        navbar .style .top = "0px";
+    } else{
+        navbar .style .top = "40px";
+    }
+});
