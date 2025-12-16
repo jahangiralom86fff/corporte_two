060b4977ba192dba017638a633c46edfe06fc402
jahangiralom86ffff <jahangiralom144155160@gmail.com>
2025-12-16 17:37:15 +0600

    all file add

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -7,12 +7,16 @@ $(document).ready(function () {
     });
 });
 
-window.addEventListener("scroll", function (){
+const navbar = document.querySelector(".header .main-navbar");
 
-    const navbar = document .querySelector(".header .main-navbar");
-    if( window .scrollY > 40){
-        navbar .style .top = "0px";
-    } else{
-        navbar .style .top = "40px";
+function updateNavbar() {
+    if (window.scrollY > 40) {
+        navbar.style.top = "0px";
+    } else {
+        navbar.style.top = "40px";
     }
-});
+}
+
+window.addEventListener("load", updateNavbar);
+window.addEventListener("scroll", updateNavbar);
+; 
